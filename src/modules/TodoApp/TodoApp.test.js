3eac97e4e5fe3f494b562fdc1292cc32c70024eb
Joseph Higgins <joseph@jhiggins.org>
2017-08-08 18:23:25 -0400

    Fix spacing

--- src/modules/TodoApp/TodoApp.test.js
+++ src/modules/TodoApp/TodoApp.test.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {mount, shallow} from 'enzyme';
+
 import TodoApp from './TodoApp';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<TodoApp />, div);
+    mount(<TodoApp />);
+});
+
+it('renders a title', () => {
+
+    const todoApp = shallow((
+        <TodoApp />
+    ));
+
+    expect(todoApp.find('h1')).toHaveLength(1);
 });
